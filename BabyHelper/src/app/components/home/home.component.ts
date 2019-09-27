a6f8acb06fc1138fd3e3990969d318b877b35a50
Sander <s.vanmaurik@student.fontys.nl>
2019-09-27 12:11:41 +0200

    Added more categories

--- BabyHelper/src/app/components/home/home.component.ts
+++ BabyHelper/src/app/components/home/home.component.ts
@@ -11,11 +11,17 @@ export class HomeComponent implements OnInit {
   private categories : Category[] = [];
 
   private babyName: string = "Test";
+  private babyBirthDate: string = "27-9-2018";
   constructor() { }
 
   ngOnInit() {
     this.categories.push(new Category("Baby", "What did "+ this.babyName + " do today?"));
-    this.categories.push(new Category("SoundBoard", "Is " + this.babyName + " having trouble sleeping? Try some of these sounds!" ));
+    this.categories.push(new Category("SoundBoard", "Try the soundboard!" ));
+    this.categories.push(new Category("Camera", "IP-Camera connectivity"));
+    this.categories.push(new Category("Illness", "What diseases could "+ this.babyName + " get?"));
+    this.categories.push(new Category("News", "Recent news about babies"));
+    this.categories.push(new Category("Toys", "Recommended toys for babies"));
+
   }
 
 }
